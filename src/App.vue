2e89b19e63982beb35820029746938162887b611
ahmet989g <ahmet.erden98@gmail.com>
2022-09-13 01:19:04 +0300

    vue-income-tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <app-header :totalIncome="state.totalIncome" />
+  <app-form @add-income="AddIncome" />
+  <income-list :state="state" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader';
+import AppForm from './components/AppForm.vue';
+import IncomeList from './components/IncomeList.vue';
+import { computed, reactive } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  
+  setup() {
+    const state = reactive({
+      income: [],
+      totalIncome: computed(() => {
+        let temp = 0;
+        if(state.income.length > 0){
+          for(let i = 0; i < state.income.length; i++){
+            temp += state.income[i].value;
+          }
+        }
+        return temp;
+      }),
+      sortedIncome: computed(() => {
+        let temp = [];
+
+        temp = state.income.slice().sort(function (a, b) {
+          return new Date(b.date) - new Date(a.date);
+        });
+
+        return temp;
+      })
+    });
+
+    function AddIncome(data){
+      let d = data.date.split("-");
+      let newD = new Date(d[0], d[1], d[2]);
+
+      state.income = [...state.income,{
+        id:Date.now(),
+        desc: data.desc,
+        value: parseInt(data.value),
+        date: newD.getTime()
+      }];
+
+    }
+
+    return{
+      state,
+      AddIncome
+    }
+  },
+  components:{
+    AppHeader,
+    AppForm,
+    IncomeList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body{
+  background: #EEE;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <h1>Income Tracker</h1>
+    <div class="total-income">${{totalIncome}}</div>
+  </header>
+</template>
+
+<script>
+export default {
+  props:{
+    totalIncome: Number
+  }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 30px;
+        background-color: #313131;
+        border-bottom: 5px solid #fda335;
+    }
+    header h1{
+        color: #EEE;
+        font-size: 28px;
+    }
+    header .total-income{
+        font-family: 'Fira Code', 'Fira Sans', sans-serif;
+        background-color: #fda335;
+        color: #FFF;
+        font-size: 20px;
+        font-weight: 900;
+        padding: 5px 10px;
+        min-width: 100px;
+        text-align: center;
+        border-radius: 8px;
+        box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.25);
+        text-shadow: inset 0 3px 3px rgba(0, 0, 0, 0.25);
+    }
+</style>
